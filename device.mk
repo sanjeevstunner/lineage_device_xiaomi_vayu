4719ba35e3e21317833d5cb30c51b072145b159f
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-08 14:11:45 -0400

    vayu: display: Ship displayconfig for brightness ramp rate
    Android 12 allows the brightness ramp rate to be changed separately
    for fast/slow and increase/decrease in float. Once loaded,
    DisplayDeviceConfig has higher priority than the integer config
    values in frameworks. The format definition is in [1].
    
    There are 3 variants for the filename:
    
    * display_id_46xxxxx.xml
    * display_19xxxxx.xml
    * display_port_xxx.xml
    
    The value can be found in boot logs by grepping DisplayDeviceInfo.
    On android 11, boot log reports the 19xxxxx value of the second
    variant. On android 12, boot log reports the 46xxxxx value of the
    first variant.
    
    The xml file can be put in either product or vendor.
    
    [1] frameworks/base/services/core/xsd/display-device-config/display-device-config.xsd

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/camera/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/display/display_id_4630946736638489729.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946736638489729.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     libkeymaster_messages.vendor
