1288aa9b119bf424f3b24e1451dcbf69f0b5bdb2
jhenrique09 <jhsv09@gmail.com>
2022-01-05 11:57:37 -0300

    vayu: Enable quick tap

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -14,6 +14,9 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
+# Feature Flags
+TARGET_SUPPORTS_QUICK_TAP := true
+
 PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
