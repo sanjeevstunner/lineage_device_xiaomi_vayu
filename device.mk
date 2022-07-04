41503f3a7691395993df67469104d8082a2efbf9
Sanjeev <sanjeevstunner1@gmail.com>
2022-07-04 17:03:33 +0530

    vayu: Drop overlay-lineage
    *these configs aren't used by Pixel Experience
    
    Revert "vayu: overlay-lineage: Define fingerprint sensor location"
    This reverts commit 24fadadd2de3ff297f2df344cbb7f7325fec91e9.
    
    Revert "vayu: overlay-lineage: config_haveNotch should be true"
    This reverts commit 7c02b8adec4ea640c95bd73402ac71f06e6940fc.
    
    Revert "vayu: overlay-lineage: set config_haveNotch overlay to true"
    This reverts commit 4a23d7a4408b3b5cf06fcd8973f2cfb9ddadf32d.
    
    Revert "vayu: overlay-lineage: Enable min_refresh_rate setting"
    This reverts commit 92d1916eed22fa77a4f4b85834410274d4e4cbcb.

--- device.mk
+++ device.mk
@@ -34,9 +34,7 @@ PRODUCT_PACKAGES += \
     libkeymaster_messages.vendor
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
